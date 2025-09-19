30baebd37c170251f7c9ad81f7979f262ef46426
djw <1067095472@qq.com>
2025-09-19 17:02:45 +0800

    FEAT: 添加react项目及部分配置

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true, // 指定此配置文件为根配置，ESLint 将不会查找父目录中的配置文件
+  env: {
+    node: true, // 启用 Node.js 环境
+    browser: true, // 启用浏览器环境
+    es2021: true // 启用 ES6 的语法
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // 规则
+    '@typescript-eslint/no-var-requires': 'off'
+  }
+};
